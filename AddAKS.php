1af31dbf396e59f86aca6eac373c0e1b49729888
getjaked <alexander.aberg2@elev.ga.ntig.se>
2022-04-05 08:54:51 +0200

    Ny commit

--- AddAKS.php
+++ AddAKS.php
@@ -1,1 +1,14 @@
-Nu ska jag jobba
+<?php
+
+$servername "";
+$username = "root";
+$password = "";
+$dbname = "";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("connection failed: " . $conn->connect_error);
+}
+
+?>
