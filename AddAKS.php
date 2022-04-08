5a0a55384b0a5177bbda0daab799f9ecfdf39a44
getjaked <alexander.aberg2@elev.ga.ntig.se>
2022-04-08 12:03:25 +0200

    Insert Into

--- AddAKS.php
+++ AddAKS.php
@@ -1,14 +1,25 @@
 <?php
 
-$servername "";
+$servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "";
+$dbname = "ntigskov_go2";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("connection failed: " . $conn->connect_error);
 }
+$X = NULL;
+$sqla = "INSERT INTO `anvandare` (`Namn`) VALUES ($X)";
+$sqlk = "INSERT INTO `kategorier` (`Kategorier`) VALUES ()";
+$sqls = "INSERT INTO `spel` (`Spel`) VALUES ()";
 
+if ($conn->query($sqla) === TRUE) {
+    echo "Clone created";
+}   else {
+    echo "Error no go: " . $conn->error;
+}
+
+$conn->close();
 ?>
